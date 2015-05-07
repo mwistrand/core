47e685e20ce2b50ae794f161c46815760f0a1cc5
James Donaghue <james.donaghue@gmail.com>
2015-05-07 07:33:02 -0400

    implement string.repeat shim

--- src/string.ts
+++ src/string.ts
@@ -35,6 +35,25 @@ function getPadding(text: string, size: number, character: string = '0'): string
 	return padding;
 }
 
+function validateSubstringComparisonArguments(value: any, search: any): void {
+	if (value == null) {
+		throw new TypeError();
+	}
+
+	if (Object.prototype.toString.call(search) === '[object RegExp]') {
+		throw new TypeError();
+	}
+}
+
+function substringCompare(value: string, search: string, start: number, length: number): boolean {
+	for (let i = start; i < length; i++) {
+		if (value[i] !== search[i - start]) {
+			return false
+		}
+	}
+	return true;
+}
+
 export function escapeRegExp(text: string): string {
 	return !text ? text : text.replace(escapeRegExpPattern, '\\$&');
 }
@@ -73,4 +92,136 @@ export function padEnd(text: string, size: number, character: string = '0'): str
 	}
 
 	return text + getPadding(text, size, character);
+}
+
+/**
+ * Repeats the string coerced value n times.
+ * @return The repeated string coerced value
+ */
+export function repeat(value: any, times: number): string {
+	times = Math.floor(+times);
+	if (times != times || times === 0) {
+		return '';
+	}
+
+	// check for range errors
+	if (times < 0 || times >= Infinity) {
+		throw new RangeError('repeat times value must be within the following boundaries (0, Infinity]');
+	}
+
+	// http://jsperf.com/string-repeat2/12
+	value = String(value);
+	let accumulator = value;
+	let remainderAccumulator = '';
+	let remainderAccumulatorCount = 0;
+
+	let base2Exp = Math.log(times) / Math.log(2);
+	let base2ExpInt = Math.floor(base2Exp);
+	let remainder = times - Math.pow(2, base2ExpInt);
+
+	for (let i = 0; i < base2ExpInt; i++) {
+		accumulator += accumulator;
+
+		if (remainder > 0) {
+			remainderAccumulatorCount += remainderAccumulatorCount === 0 ? 1 : remainderAccumulatorCount;
+			if (remainderAccumulatorCount > remainder || i + 1 === base2ExpInt) {
+				for (let j = 0; j < remainder; j++) {
+					remainderAccumulator += value;
+				}
+				remainder = 0;
+			}
+			else {
+				remainderAccumulator += remainderAccumulator ? remainderAccumulator : value;
+				remainder -= remainderAccumulatorCount === 1 ? 1 : (remainderAccumulatorCount / 2);
+			}
+		}
+	}
+
+	return accumulator + remainderAccumulator;
+};
+
+/**
+ * Determines whether a string begins with the characters and another string
+ * @return a boolean indicating the truth of the comparison
+ */
+export function startsWith(value: any, search: any, position: number = 0): boolean {
+	validateSubstringComparisonArguments(value, search);
+
+	value = String(value);
+	search = String(search);
+	position = Math.floor(+position);
+
+	if (position != position) {
+		position = 0;
+	}
+
+	let valueLength = value.length;
+	let searchLength = search.length;
+	let start = Math.min(Math.max(position, 0), valueLength);
+
+	if (searchLength + position > valueLength) {
+		return false;
+	}
+
+	return substringCompare(value, search, start, searchLength + position);
+};
+
+/**
+ * Determines whether a string includes the characters and another string
+ * @return a boolean indicating the truth of the comparison
+ */
+export function includes(value: any, search: string, position: number = 0): boolean {
+	validateSubstringComparisonArguments(value, search);
+
+	value = String(value);
+	search = String(search);
+
+	position = Math.floor(+position);
+
+	if (position != position) {
+		position = 0;
+	}
+
+	let valueLength = value.length;
+	let searchLength = search.length;
+	let start = Math.min(Math.max(position, 0), valueLength);
+
+	if (searchLength + position > valueLength) {
+		return false;
+	}
+
+	return value.indexOf(search, position) !== -1;
+}
+
+/**
+ * Determines whether a string ends with the characters and another string
+ * @return a boolean indicating the truth of the comparison
+ */
+export function endsWith(value: any, search: string, position?: number): boolean {
+	validateSubstringComparisonArguments(value, search);
+
+	value = String(value);
+	search = String(search);
+
+	let valueLength = value.length;
+	let searchLength = search.length;
+
+	if (position == null || position >= Infinity) {
+		position = valueLength;
+	}
+
+	position = Math.floor(+position);
+
+	if (position != position) {
+		position = valueLength;
+	}
+
+	let end = Math.min(Math.max(position, 0), valueLength);
+	let start = end - searchLength;
+
+	if (start < 0) {
+		return false;
+	}
+
+	return substringCompare(value, search, start, position);
 }
--- tests/unit/string.ts
+++ tests/unit/string.ts
@@ -1,6 +1,6 @@
 import registerSuite = require('intern!object');
 import assert = require('intern/chai!assert');
-import {escapeRegExp, escapeXml, padStart, padEnd} from 'src/string';
+import {escapeRegExp, escapeXml, padStart, padEnd, repeat, startsWith, includes, endsWith} from 'src/string';
 
 registerSuite({
 	name: 'string functions',
@@ -46,5 +46,310 @@ registerSuite({
 		assert.throw(function () {
 			padEnd('Lorem', Infinity);
 		}, RangeError);
+	},
+
+	'.repeat()': {
+		'throws on negative count'() {
+			assert.throws(function () {
+				repeat('abc', -1);
+			});
+		},
+
+		'throws on Infinity count'() {
+			assert.throws(function () {
+				repeat('abc', Infinity);
+			});
+		},
+
+		'repeats the expected number of times'() {
+			assert.strictEqual(repeat('abc', 3), 'abcabcabc');
+		},
+
+		'coerces count to integer'() {
+			assert.strictEqual(repeat('abc', 7.8), 'abcabcabcabcabcabcabc');
+		},
+
+		'repeats 0 times when passed a count that coerces to 0 or NaN'() {
+			var counts = [ NaN, undefined, null, 0, false, 'abc' ];
+			for (var i = counts.length; i--;) {
+				assert.strictEqual(repeat('abc', <any> counts[i]), '');
+			}
+		},
+
+		'coerces target to string'() {
+			assert.strictEqual(repeat(undefined, 4), 'undefinedundefinedundefinedundefined');
+			assert.strictEqual(repeat(false, 3), 'falsefalsefalse');
+			assert.strictEqual(repeat(1, 3), '111');
+		}
+	},
+
+	'.startsWith()': {
+		'throws on null value or RegExp'() {
+			assert.throws(function () {
+				startsWith(undefined, 'abc');
+			});
+			assert.throws(function () {
+				startsWith(null, 'abc');
+			});
+			assert.throws(function () {
+				startsWith('abc', <any> /\s+/);
+			});
+		},
+
+		'null or undefined search value'() {
+			assert.isTrue(startsWith('undefined', undefined));
+			assert.isFalse(startsWith('undefined', null));
+			assert.isTrue(startsWith('null', null));
+			assert.isFalse(startsWith('null', undefined));
+		},
+
+		'position not included or coerced to NaN'() {
+			var counts = [ NaN, undefined, null, 'abc' ];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(startsWith('abc', undefined, <any> counts[i]));
+				assert.isTrue(startsWith('abc', '', <any> counts[i]));
+				assert.isFalse(startsWith('abc', '\0', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'a', <any> counts[i]));
+				assert.isFalse(startsWith('abc', 'b', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'ab', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'abc', <any> counts[i]));
+				assert.isFalse(startsWith('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'position is 0'() {
+			var counts = [+0, -0];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(startsWith('abc', undefined, <any> counts[i]));
+				assert.isTrue(startsWith('abc', '', <any> counts[i]));
+				assert.isFalse(startsWith('abc', '\0', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'a', <any> counts[i]));
+				assert.isFalse(startsWith('abc', 'b', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'ab', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'abc', <any> counts[i]));
+				assert.isFalse(startsWith('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'position is Infinity'() {
+			assert.isFalse(startsWith('abc', undefined, Infinity));
+			assert.isFalse(startsWith('abc', '', Infinity));
+			assert.isFalse(startsWith('abc', '\0', Infinity));
+			assert.isFalse(startsWith('abc', 'a', Infinity));
+			assert.isFalse(startsWith('abc', 'b', Infinity));
+			assert.isFalse(startsWith('abc', 'ab', Infinity));
+			assert.isFalse(startsWith('abc', 'abc', Infinity));
+			assert.isFalse(startsWith('abc', 'abcd', Infinity));
+		},
+
+		'position is 1 or coerced to 1'() {
+			var counts = [ 1, true ];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(startsWith('abc', undefined, <any> counts[i]));
+				assert.isTrue(startsWith('abc', '', <any> counts[i]));
+				assert.isFalse(startsWith('abc', '\0', <any> counts[i]));
+				assert.isFalse(startsWith('abc', 'a', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'b', <any> counts[i]));
+				assert.isTrue(startsWith('abc', 'bc', <any> counts[i]));
+				assert.isFalse(startsWith('abc', 'abc', <any> counts[i]));
+				assert.isFalse(startsWith('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'value is string a non-string'() {
+			assert.isTrue(startsWith(120, '12'));
+			assert.isTrue(startsWith(true, 'tru'));
+			assert.isTrue(startsWith(false, 'fa'));
+			assert.isTrue(startsWith(NaN, 'N'));
+			assert.isTrue(startsWith(/\s+/, '/\\s'));
+		},
+
+		'unicode support'() {
+			assert.isTrue(startsWith('\xA2fa', '\xA2'));
+			assert.isTrue(startsWith('\xA2fa', 'fa', 1));
+			assert.isTrue(startsWith('\xA2fa\uDA04', '\xA2fa\uDA04'));
+			assert.isTrue(startsWith('\xA2fa\uDA04', 'fa\uDA04', 1));
+			assert.isTrue(startsWith('\xA2fa\uDA04', '\uDA04', 3));
+		}
+	},
+
+	'.includes()': {
+		'throws on null value or RegExp'() {
+			assert.throws(function () {
+				includes(undefined, 'abc');
+			});
+			assert.throws(function () {
+				includes(null, 'abc');
+			});
+			assert.throws(function () {
+				includes('abc', <any> /\s+/);
+			});
+		},
+
+		'null or undefined search value'() {
+			assert.isTrue(includes('undefined', undefined));
+			assert.isFalse(includes('undefined', null));
+			assert.isTrue(includes('null', null));
+			assert.isFalse(includes('null', undefined));
+		},
+
+		'position not included or coerced to NaN'() {
+			var counts = [ NaN, undefined, null, 'abc' ];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(includes('abc', undefined, <any> counts[i]));
+				assert.isTrue(includes('abc', '', <any> counts[i]));
+				assert.isFalse(includes('abc', '\0', <any> counts[i]));
+				assert.isTrue(includes('abc', 'a', <any> counts[i]));
+				assert.isTrue(includes('abc', 'b', <any> counts[i]));
+				assert.isTrue(includes('abc', 'ab', <any> counts[i]));
+				assert.isTrue(includes('abc', 'abc', <any> counts[i]));
+				assert.isFalse(includes('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'position is 0'() {
+			var counts = [+0, -0];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(includes('abc', undefined, <any> counts[i]));
+				assert.isTrue(includes('abc', '', <any> counts[i]));
+				assert.isFalse(includes('abc', '\0', <any> counts[i]));
+				assert.isTrue(includes('abc', 'a', <any> counts[i]));
+				assert.isTrue(includes('abc', 'b', <any> counts[i]));
+				assert.isTrue(includes('abc', 'ab', <any> counts[i]));
+				assert.isTrue(includes('abc', 'abc', <any> counts[i]));
+				assert.isFalse(includes('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'position is Infinity'() {
+			assert.isFalse(includes('abc', undefined, Infinity));
+			assert.isFalse(includes('abc', '', Infinity));
+			assert.isFalse(includes('abc', '\0', Infinity));
+			assert.isFalse(includes('abc', 'a', Infinity));
+			assert.isFalse(includes('abc', 'b', Infinity));
+			assert.isFalse(includes('abc', 'ab', Infinity));
+			assert.isFalse(includes('abc', 'abc', Infinity));
+			assert.isFalse(includes('abc', 'abcd', Infinity));
+		},
+
+		'position is 1 or coerced to 1'() {
+			var counts = [ 1, true ];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(includes('abc', undefined, <any> counts[i]));
+				assert.isTrue(includes('abc', '', <any> counts[i]));
+				assert.isFalse(includes('abc', '\0', <any> counts[i]));
+				assert.isFalse(includes('abc', 'a', <any> counts[i]));
+				assert.isTrue(includes('abc', 'b', <any> counts[i]));
+				assert.isTrue(includes('abc', 'bc', <any> counts[i]));
+				assert.isFalse(includes('abc', 'abc', <any> counts[i]));
+				assert.isFalse(includes('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'value is string a non-string'() {
+			assert.isTrue(includes(120, '12'));
+			assert.isTrue(includes(true, 'tru'));
+			assert.isTrue(includes(false, 'fa'));
+			assert.isTrue(includes(NaN, 'N'));
+			assert.isTrue(includes(/\s+/, '/\\s'));
+		},
+
+		'unicode support'() {
+			assert.isTrue(includes('\xA2fa', '\xA2'));
+			assert.isTrue(includes('\xA2fa', 'fa', 1));
+			assert.isTrue(includes('\xA2fa\uDA04', '\xA2fa\uDA04'));
+			assert.isTrue(includes('\xA2fa\uDA04', 'fa\uDA04', 1));
+			assert.isTrue(includes('\xA2fa\uDA04', '\uDA04', 3));
+		}
+	},
+
+	'.endsWith()': {
+		'throws on null value or RegExp'() {
+			assert.throws(function () {
+				endsWith(undefined, 'abc');
+			});
+			assert.throws(function () {
+				endsWith(null, 'abc');
+			});
+			assert.throws(function () {
+				endsWith('abc', <any> /\s+/);
+			});
+		},
+
+		'null or undefined search value'() {
+			assert.isTrue(endsWith('undefined', undefined));
+			assert.isFalse(endsWith('undefined', null));
+			assert.isTrue(endsWith('null', null));
+			assert.isFalse(endsWith('null', undefined));
+		},
+
+		'position not included or coerced to NaN'() {
+			var counts = [ NaN, undefined, null, 'abc' ];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(endsWith('abc', undefined, <any> counts[i]));
+				assert.isTrue(endsWith('abc', '', <any> counts[i]));
+				assert.isFalse(endsWith('abc', '\0', <any> counts[i]));
+				assert.isTrue(endsWith('abc', 'c', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'b', <any> counts[i]));
+				assert.isTrue(endsWith('abc', 'bc', <any> counts[i]));
+				assert.isTrue(endsWith('abc', 'abc', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'position is 0'() {
+			var counts = [+0, -0];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(endsWith('abc', undefined, <any> counts[i]));
+				assert.isTrue(endsWith('abc', '', <any> counts[i]));
+				assert.isFalse(endsWith('abc', '\0', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'a', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'b', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'ab', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'abc', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'position is Infinity'() {
+			assert.isFalse(endsWith('abc', undefined, Infinity));
+			assert.isTrue(endsWith('abc', '', Infinity));
+			assert.isFalse(endsWith('abc', '\0', Infinity));
+			assert.isTrue(endsWith('abc', 'c', Infinity));
+			assert.isFalse(endsWith('abc', 'b', Infinity));
+			assert.isTrue(endsWith('abc', 'bc', Infinity));
+			assert.isTrue(endsWith('abc', 'abc', Infinity));
+			assert.isFalse(endsWith('abc', 'abcd', Infinity));
+		},
+
+		'position is 1 or coerced to 1'() {
+			var counts = [ 1, true ];
+			for (var i = counts.length; i--;) {
+				assert.isFalse(endsWith('abc', undefined, <any> counts[i]));
+				assert.isTrue(endsWith('abc', '', <any> counts[i]));
+				assert.isFalse(endsWith('abc', '\0', <any> counts[i]));
+				assert.isTrue(endsWith('abc', 'a', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'b', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'bc', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'abc', <any> counts[i]));
+				assert.isFalse(endsWith('abc', 'abcd', <any> counts[i]));
+			}
+		},
+
+		'value is string a non-string'() {
+			assert.isTrue(endsWith(120, '20'));
+			assert.isTrue(endsWith(true, 'rue'));
+			assert.isTrue(endsWith(false, 'lse'));
+			assert.isTrue(endsWith(NaN, 'N'));
+			assert.isTrue(endsWith(/\s+/, '\\s+/'));
+		},
+
+		'unicode support'() {
+			assert.isTrue(endsWith('\xA2fa\xA3', 'fa\xA3'));
+			assert.isTrue(endsWith('\xA2fa', '\xA2', 1));
+			assert.isTrue(endsWith('\xA2fa\uDA04', '\xA2fa\uDA04'));
+			assert.isTrue(endsWith('\xA2fa\uDA04', 'fa', 3));
+			assert.isTrue(endsWith('\xA2fa\uDA04', '\uDA04'));
+		}
 	}
 });
